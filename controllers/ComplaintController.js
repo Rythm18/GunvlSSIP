e3ac7bea287301f9c1ce7edd66b156095688de9b
Ankit <ankitpandey21@gnu.ac.in>
2023-11-24 22:36:50 +0530

    loginnnnnnn

--- controllers/ComplaintController.js
+++ controllers/ComplaintController.js
@@ -1,5 +1,5 @@
 const _ = require('lodash')
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 const { Complaint, validateComplaint } = require('../models/complaint')
 
@@ -8,63 +8,48 @@ const { Complaint, validateComplaint } = require('../models/complaint')
  * @description add complaint by user
  * @type RequestHandler
  */
-const verifie = 
-  [
-    {"JZRM5981": "mohatadhruv@gmail.com"},
-    {"JZRM5982":"dhirajmohata86@gmial.com"},
-    {"JZRM5983": "ridhamk.j@gmail.com"},
-    {"JZRM5984" : "mohatadhruv@gmail.com"},
-    {"JZRM5985": "mohatadhruv@gmail.com"},
-    {"JZRM5986":"dhirajmohata86@gmial.com"},
-    {"JZRM5987": "mohatadhruv@gmail.com"},
-    {"JZRM5988":"dhirajmohata86@gmial.com"},
-    {"JZRM5989": "mohatadhruv@gmail.com"},
-    {"JZRM5990":"dhirajmohata86@gmial.com"}
-  ];
+const verifie = [
+  { JZRM5981: 'mohatadhruv@gmail.com' },
+  { JZRM5982: 'dhirajmohata86@gmial.com' },
+  { JZRM5983: 'ridhamk.j@gmail.com' },
+  { JZRM5984: 'mohatadhruv@gmail.com' },
+  { JZRM5985: 'mohatadhruv@gmail.com' },
+  { JZRM5986: 'dhirajmohata86@gmial.com' },
+  { JZRM5987: 'mohatadhruv@gmail.com' },
+  { JZRM5988: 'dhirajmohata86@gmial.com' },
+  { JZRM5989: 'mohatadhruv@gmail.com' },
+  { JZRM5990: 'dhirajmohata86@gmial.com' }
+]
 
-
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'turbogeek641@gmail.com',
-        pass: 'vcle vjly tzln yznv'
-    }
-});
-
-
-
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'turbogeek641@gmail.com',
+    pass: 'vcle vjly tzln yznv'
+  }
+})
 
 exports.addComplaint = async (req, res) => {
   const { citizenship, description } = req.body
   const { error } = validateComplaint(req.body)
 
-  
-
-  var flag = 0;
-  var ml;
-  for (const obj of verifie) 
-  {
+  var flag = 0
+  var ml
+  for (const obj of verifie) {
     for (const key in obj) {
       if (key === citizenship) {
-        flag = 1;
-        ml = obj[key];
-        break;
+        flag = 1
+        ml = obj[key]
+        break
       }
-    }       
+    }
   }
 
- 
-
-  if(flag === 0)
-  {
-    await req.flash(
-      'error',
-      'You are not a valid user please register first'
-    )
+  if (flag === 0) {
+    await req.flash('error', 'You are not a valid user please register first')
 
     res.redirect('/')
-  }
-  else {
+  } else {
     if (error) {
       res.render('index.ejs', {
         errors: error.details[0].message,
@@ -84,15 +69,15 @@ exports.addComplaint = async (req, res) => {
         to: ml,
         subject: 'Hello, you have sussesfully submited your complain',
         text: `Complaint sent succesfully ! Copy the Reference ID below: ${newComplaint._id}`
-      };
+      }
 
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
-            console.log('Error sending email: ' + error);
+          console.log('Error sending email: ' + error)
         } else {
-            console.log('Email sent: ' + info.response);
+          console.log('Email sent: ' + info.response)
         }
-    });
+      })
 
       await req.flash(
         'success_msg',
--- views/trans.js
+++ views/trans.js
@@ -1,54 +1,74 @@
 $('document').ready(function () {
-
-
-    // RESTYLE THE DROPDOWN MENU
-$('#google_translate_element').on("click", function () {
-
+  // RESTYLE THE DROPDOWN MENU
+  $('#google_translate_element').on('click', function () {
     // Change font family and color
-    $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
-        .css({
-            'color': '#000',
-            'font-family': 'Manrope',
-                            'width':'100%'
-        });
+    $('iframe')
+      .contents()
+      .find(
+        '.goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *'
+      )
+      .css({
+        color: '#000',
+        'font-family': 'Manrope',
+        width: '100%'
+      })
     // Change menu's padding
-    $("iframe").contents().find('.goog-te-menu2-item-selected').css ('display', 'none');
-        
-            // Change menu's padding
-    $("iframe").contents().find('.goog-te-menu2').css ('padding', '0px');
-  
+    $('iframe')
+      .contents()
+      .find('.goog-te-menu2-item-selected')
+      .css('display', 'none')
+
+    // Change menu's padding
+    $('iframe').contents().find('.goog-te-menu2').css('padding', '0px')
+
     // Change the padding of the languages
-    $("iframe").contents().find('.goog-te-menu2-item div').css('padding', '24px');
-  
+    $('iframe')
+      .contents()
+      .find('.goog-te-menu2-item div')
+      .css('padding', '24px')
+
     // Change the width of the languages
-    $("iframe").contents().find('.goog-te-menu2-item').css('width', '100%');
-    $("iframe").contents().find('td').css('width', '100%');
-  
+    $('iframe').contents().find('.goog-te-menu2-item').css('width', '100%')
+    $('iframe').contents().find('td').css('width', '100%')
+
     // Change hover effects
-    $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
-        $(this).css('background-color', '#151515').find('span.text').css('color', 'white');
-    }, function () {
-        $(this).css('background-color', 'white').find('span.text').css('color', '#000');
-    });
+    $('iframe')
+      .contents()
+      .find('.goog-te-menu2-item div')
+      .hover(
+        function () {
+          $(this)
+            .css('background-color', '#151515')
+            .find('span.text')
+            .css('color', 'white')
+        },
+        function () {
+          $(this)
+            .css('background-color', 'white')
+            .find('span.text')
+            .css('color', '#000')
+        }
+      )
 
     // Change Google's default blue border
-    $("iframe").contents().find('.goog-te-menu2').css('border', 'none');
+    $('iframe').contents().find('.goog-te-menu2').css('border', 'none')
 
     // Change the iframe's box shadow
-    $(".goog-te-menu-frame").css('box-shadow', '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3)');
-    
-  
-  
+    $('.goog-te-menu-frame').css(
+      'box-shadow',
+      '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3)'
+    )
+
     // Change the iframe's size and position?
-    $(".goog-te-menu-frame").css({
-        'height': '100%',
-        'width': '100%',
-        'top': '0px'
-    });
+    $('.goog-te-menu-frame').css({
+      height: '100%',
+      width: '100%',
+      top: '0px'
+    })
     // Change iframes's size
-    $("iframe").contents().find('.goog-te-menu2').css({
-        'height': '100%',
-        'width': '100%'
-    });
-});
-});
+    $('iframe').contents().find('.goog-te-menu2').css({
+      height: '100%',
+      width: '100%'
+    })
+  })
+})
